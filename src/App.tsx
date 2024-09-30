0aa5ec8fa35af6b05dd71f8d225e747e5a553672
shimseohyun <shim020924@gmail.com>
2024-09-30 23:22:53 +0900

    [Feature] #3 - Adding Class Tablet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // styles
 import { GlobalStyle, theme } from "@linenow/design-system";
+import LocalStyle from "@styles/local";
 import { ThemeProvider } from "styled-components";
 
 // components
@@ -11,10 +12,10 @@ import router from "./routes/router";
 
 // react-query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import LocalStyle from "@styles/local";
 
 function App() {
   const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const deviceType = "tablet";
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  rootElement.className = deviceType;
+
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
