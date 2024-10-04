c05b076b30148d98d5b5792d55bbc64c412f16f0
ZZZINU <nnjin987@gmail.com>
2024-10-05 00:18:16 +0900

    [Feature] #10 - Setting Login API

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const instance = axios.create({
-  baseURL: '',
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: false, //크로스 도메인 요청 시 쿠키, HTTP 인증 및 클라이언트 SSL 인증서를 사용하도록 허용한다.
 });
 
@@ -25,9 +25,9 @@ export const postResponse = async <T>(
     const response = await instance.post<T>(url, data);
     return response.data;
   } catch (error) {
-    // const axiosError = error as AxiosError;
-    // console.log(`[POST] ${url} - Data:`, data);
-    // console.error('Response error:', axiosError);
+    const axiosError = error as AxiosError;
+    console.log(`[POST] ${url} - Data:`, data);
+    console.error('Response error:', axiosError);
     return null;
   }
 };
