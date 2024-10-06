bb82d1c6afc51dd7a6024da4cfec8c6de80a379c
ZZZINU <nnjin987@gmail.com>
2024-10-06 11:02:38 +0900

    [Feature] #10 - Apply Request Interceptors

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,13 +1,18 @@
 import axios, { AxiosError } from 'axios';
-import {
-  LogoutRequest,
-  LogoutResponse,
-  TransformedLogoutResponse,
-} from './domains/logout/_interfaces';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: false, //크로스 도메인 요청 시 쿠키, HTTP 인증 및 클라이언트 SSL 인증서를 사용하도록 허용한다.
+});
+
+instance.interceptors.request.use(config => {
+  const accessToken = sessionStorage.getItem('accessToken');
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
 
 export const getResponse = async <T>(url: string): Promise<T | null> => {
@@ -24,11 +29,10 @@ export const getResponse = async <T>(url: string): Promise<T | null> => {
 
 export const postResponse = async <T>(
   url: string,
-  data: any,
-  headers?: Record<string, string>
+  data: any
 ): Promise<T | null> => {
   try {
-    const response = await instance.post<T>(url, data, { headers });
+    const response = await instance.post<T>(url, data);
     return response.data;
   } catch (error) {
     const axiosError = error as AxiosError;
