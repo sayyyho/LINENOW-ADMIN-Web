e697d9ad78480853ebed5163ee65eb1fdeb8ac20
shimseohyun <shim020924@gmail.com>
2024-10-06 12:52:54 +0900

    [Feature] #14 - Creating Navigation UI

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,11 @@
 import * as S from "./Navigation.styled";
 
 const Navigation = () => {
-  return <S.NavigationWrapper>navigation</S.NavigationWrapper>;
+  return (
+    <S.NavigationWrapper>
+      <img src="/images/image_vertical_logo.svg" />
+    </S.NavigationWrapper>
+  );
 };
 
 export default Navigation;
